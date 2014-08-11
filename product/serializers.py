ed4ccb5fc1b7f6191d396dd13f1a2fa88c1df164
Juan Gutierrez <juanny.gee@gmail.com>
2014-08-11 07:02:35 -0400

    add merchant to product serializer

--- product/serializers.py
+++ product/serializers.py
@@ -5,4 +5,4 @@ from .models import Product
 class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
-        fields = ('id', 'name', 'price')
+        fields = ('id', 'merchant', 'name', 'price')
